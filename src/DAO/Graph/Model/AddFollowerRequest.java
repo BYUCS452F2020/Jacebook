63d8c5dec143fa494263080081fa096ff72db9fb
Hoon Lee <hoon6565@gmail.com>
2020-12-04 18:20:22 -0700

    update

---
+++ src/DAO/Graph/Model/AddFollowerRequest.java
@@ -0,0 +1,6 @@
+package DAO.Graph.Model;
+
+public class AddFollowersRequest {
+    public String alias;
+    public String followAlias;
+}
---
+++ src/DAO/Graph/Model/RemoveFollowingRequest.java
@@ -0,0 +1,4 @@
+package DAO.Graph.Model;
+
+public class RemoveFollowingRequest {
+}
